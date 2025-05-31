77908de18c142dcb6968930d16e5f18eb5bd1b8b
itaitg <itaitg@gmail.com>
2025-05-31 15:45:55 +0300

    first upload

---
+++ src/AudioManager.cpp
@@ -0,0 +1,24 @@
+//
+// Created by itait on 31/05/2025.
+//
+
+#include <iostream>
+
+#include "AudioManager.hpp"
+
+using namespace std;
+
+void AudioManager::Recieveaudio()
+{
+    cout << "Recieving audio" << endl;
+}
+
+void AudioManager::Sendaudio()
+{
+    cout << "Sending audio" << endl;
+}
+
+void AudioManager::Disconnectaudio()
+{
+    cout << "Disconnecting audio" << endl;
+}
