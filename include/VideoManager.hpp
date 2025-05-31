57b5a2b454bd3384c3c9bae512623812b78be498
itaitg <itaitg@gmail.com>
2025-05-31 17:36:27 +0300

    added basic for testing

---
+++ include/VideoManager.hpp
@@ -0,0 +1,19 @@
+//
+// Created by itait on 31/05/2025.
+//
+
+#ifndef VIDEOMANAGER_HPP
+#define VIDEOMANAGER_HPP
+
+
+class VideoManager
+{
+public:
+    void Videorecieve();
+    void Videodisconnect();
+
+private:
+};
+
+
+#endif //VIDEOMANAGER_HPP
